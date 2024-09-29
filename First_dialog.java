b0e6d53bedd913d7dd1c4084553a1da8b895680a
TruongDuke <168114893+TruongDuke@users.noreply.github.com>
2024-09-29 21:14:57 +0700

    Add files via upload

---
+++ First_dialog.java
@@ -0,0 +1,8 @@
+package the_very_first_java_programs;
+import javax.swing.*;
+public class First_dialog {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hello world! How are you?");
+        System.exit(0);
+    }
+}
